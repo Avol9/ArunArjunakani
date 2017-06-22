967f7bdb556651cc7678f1a009b314b120fc01da
arunarjunakani <arunarjunakani@gmail.com>
2017-06-21 20:47:38 -0500

    Update

--- project/src.js
+++ project/src.js
@@ -7,14 +7,3 @@ function trackClick()
       });
 	console.log("Hello!");
 }
-
-// var buttons = document.querySelectorAll('.btn');
-// buttons.forEach( function( btn ) {
-//    btn.addEventListener('click', function(event) {
-//       ga('send', 'event', {
-//          eventAction: 'click',
-//          eventCategory: 'Buy Now Buttons',
-//          eventLabel: 'Buy Now'
-//       });
-//    });
-// } );
