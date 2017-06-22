9ed811de85d6218dc8dea73fba1b172a339327ab
arunarjunakani <arunarjunakani@gmail.com>
2017-06-22 18:26:15 -0500

    Update

--- project/src.js
+++
@@ -1,9 +0,0 @@
-function trackClick()
-{
-	ga('send', 'event', {
-         eventAction: 'click',
-         eventCategory: 'Buy Now Buttons',
-         eventLabel: 'Buy Now'
-      });
-	console.log("Hello!");
-}
